6f53f7da6fe87ebf6a452160ea74105edbcf7db5
Sebby-T <31851799+Sebby-T@users.noreply.github.com>
2019-12-05 14:28:40 -0500

    Add javascript to the image gallery making it responsive

--- _patterns/cards/gal.js
+++ _patterns/cards/gal.js
@@ -1,22 +1,30 @@
-var $imgBtn = $('.img-btn')
+var $imgBtn = $('.img-btn');
+var $close = $('.close');
 
-$imgBtn.on('click', function(e) {
+$imgBtn.on('click', function (e) {
+  var id = $(this).attr('href');
   e.preventDefault();
-  $('.gallery-item').css('display', 'none');
-  $(this).css('display', 'block');
-  if ($imgBtn.hasClass('overlay')) {
-    $imgBtn.addClass('gallery-item');
-    $imgBtn.removeClass('overlay');
-    $('.gal-cont').addClass('embed');
-    $('.gal-cont').addClass('embed-1by1');
-    $('.gal-img').addClass('embed-item');
-    $('.gallery-item').css('display', 'block');
-    e.preventDefault();
-  } else {
-    $imgBtn.removeClass('gallery-item');
-    $imgBtn.addClass('overlay');
-    $('.gal-cont').removeClass('embed');
-    $('.gal-cont').removeClass('embed-1by1');
-    $('.gal-img').removeClass('embed-item');
+  $(id).css('display', 'block');
+  $(id).css('opacity', '1');
+  $('.img-expand').css('display', 'block');
+  $('.img-expand').css('opacity', '1');
+  $close.css('display', 'block');
+  $('body').addClass('no-scroll')
+});
+
+$close.on('click', function (e) {
+  var close = $(this).attr('href');
+  e.preventDefault();
+  $('.overlay').css('display', 'none');
+  $('.overlay').css('opacity', '0');
+  $('.img-expand').css('display', 'none');
+  $('.img-expand').css('opacity', '0');
+  $close.css('display', 'none');
+  $('body').removeClass('no-scroll')
+});
+
+$(document).on('keydown', function(e) {
+  if(e.key=='Escape'){
+    $close.click()
   }
 });
